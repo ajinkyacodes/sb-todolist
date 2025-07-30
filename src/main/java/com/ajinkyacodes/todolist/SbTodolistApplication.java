6141402c94628490f2eb0435943ce456070b976b
AJINKYA <ajinkyacodes@gmail.com>
2025-07-30 19:53:00 +0530

    setup files added

---
+++ src/main/java/com/ajinkyacodes/todolist/SbTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.ajinkyacodes.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajinkyacodes/todolist/SbTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajinkyacodes.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
